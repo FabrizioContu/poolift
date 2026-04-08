ea058f173eb8e29c3bbb9e72ac6c568f477a35b8
Fabrizio Contu <fab.contu@gmail>
2026-04-08 18:27:33 +0200

    refactor: remove SpoilerGuard from all project logic

--- components/auth/index.ts
+++ components/auth/index.ts
@@ -1,5 +1,4 @@
 // components/auth/index.ts
 export { AccessGuard } from './AccessGuard'
-export { SpoilerGuard } from './SpoilerGuard'
 export { AuthModal } from './AuthModal'
 export { UserMenu } from './UserMenu'
